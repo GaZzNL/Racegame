d9720d43336d3f4d2b7db7f556c1394ccb251389
Bril1400 <Bril1400@student.nhl.nl>
2014-10-22 14:52:56 +0200

    Changed Vehicle Class
    -Compiled
    Added BallAxisisSpeed

--- Racegame/Racegame/Vehicles.cs
+++ Racegame/Racegame/Vehicles.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Racegame
 {
-    class Vehicles
+    class Vehicle
     {
+        int Health;
+        int Maxspeed;
+        int Accl;
+        int Fuel;
+
+        public void Ballposition(int change);
+
+        const int BallAxisSpeed = 2;
+
+        Point BallPos = new Point(30, 30);
+        Point BallSpeed = new Point(BallAxisSpeed, BallAxisSpeed);
+        const int BallSize = 50;
+
     }
 }
+
