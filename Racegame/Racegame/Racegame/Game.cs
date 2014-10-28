805dcfd2a609263d50cd66b44bdf83478eac26e4
GaZzNL <robert-stocker@hotmail.com>
2014-10-28 22:49:55 +0100

    Zo goed als klaar

--- Racegame/Racegame/Racegame/Game.cs
+++ Racegame/Racegame/Racegame/Game.cs
@@ -89,7 +89,6 @@ namespace Racegame
                     vehicle1.Draw(g);
                     vehicle2.Draw(g, "red");
                 }
-
                 //hier check je of de autos niet van het scherm rijden/ Als de voertuig "Te verweg is van de baan" word hij op de start positie gezet
                 if (vehicle1.Position.X < -10 | vehicle1.Position.Y < -10 | vehicle1.Position.X > 1000 | vehicle1.Position.Y > 710)
                 {
@@ -132,19 +131,38 @@ namespace Racegame
                     vehicle2.Position.X -= vehicle2.Speed.X;
                     vehicle2.Position.Y -= vehicle2.Speed.Y;
                 }
-                
-                //De auto(als in voertuig) debugger!
-                MousePosition.Text = Convert.ToString(vehicle1.Position);
-                
-                //wat is de kleur van de huidige positie
-                //Checkt de kleur waar vehicle1 op positie staat
-                Color kleur = Backbuffer.GetPixel(vehicle1.Position.X, vehicle1.Position.Y);
-
+                //De auto(als in voertuig) debugger! deze controleerd nu op de locatie
+                MousePosition.Text = (Convert.ToString(vehicle1.Position));
+                //Tegen het kapot gaan van onze game
+                try
+                {
+                    //wat is de kleur van de huidige positie
+                    //Checkt de kleur waar vehicle1 op positie staat(We moeten echt de GUI afschafen van Visual studio :p)
+                    //Controleer de juist locatie
+                    var image = new Bitmap(@"E:\git\Racegame\Racegame\Racegame\Racegame\Resources\Naamloos.jpg");
+                    //Een if statement voor een onnodige exception
+                    if (vehicle1.Position.X < -10 | vehicle1.Position.Y < -10 | vehicle1.Position.X > 1000 | vehicle1.Position.Y > 710)
+                    {
+                        Color kleur = image.GetPixel(vehicle1.Position.X, vehicle1.Position.Y);
+                        //Debug geeft de code waar hij op staat
+                        Console.WriteLine(kleur);
+                        //Gras
+                        if (kleur.A.Equals(255) && kleur.R.Equals(0) && kleur.G.Equals(128) && kleur.B.Equals(1)) Console.WriteLine("Gras"); vehicle1.AxisSpeed = 1;
+                        //De weg
+                        if (kleur.A.Equals(255) && kleur.R.Equals(106) && kleur.G.Equals(104) && kleur.B.Equals(105)) Console.WriteLine("weg");
+                        //Pitstop
+                        if (kleur.A.Equals(255) && kleur.R.Equals(0) && kleur.G.Equals(0) && kleur.B.Equals(206)) Console.WriteLine("Pitstop");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error met de bitmap: " + e);
+                }
+            
                 Invalidate();
+              
             }
         }
-
-
     }
 }
 
