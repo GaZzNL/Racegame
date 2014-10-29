3ff12daf1fdb5d34c28b7291bcd38280ae6da381
GaZzNL <robert-stocker@hotmail.com>
2014-10-29 12:34:30 +0100

    OOP voor de win :dancer:

--- Racegame/Racegame/Racegame/Game.cs
+++ Racegame/Racegame/Racegame/Game.cs
@@ -92,7 +92,6 @@ namespace Racegame
 
             }
         }
-
         void Form1_Paint(object sender, PaintEventArgs e)
         {
             if (Backbuffer != null)
@@ -120,79 +119,13 @@ namespace Racegame
                     vehicle1.Draw(g);
                     vehicle2.Draw(g, "red");
                 }
-                //hier check je of de autos niet van het scherm rijden/ Als de voertuig "Te verweg is van de baan" word hij op de start positie gezet
-                if (vehicle1.Position.X < -10 | vehicle1.Position.Y < -10 | vehicle1.Position.X > 1000 | vehicle1.Position.Y > 710)
-                {
-                    vehicle1.Position.X = 768;
-                    vehicle1.Position.Y = 70;
-                }
-                //Zelfde geld voor voertuig2
-                if (vehicle2.Position.X < -10 | vehicle2.Position.Y < -10 | vehicle2.Position.X > 1000 | vehicle2.Position.Y > 710)
-                {
-                    vehicle2.Position.X = 768;
-                    vehicle2.Position.Y = 70;
-                }
-                //Hier word gecheckt als de voertuig zich mag verplaatsen op de locatie
-                if (vehicle2.Position.X < 0 & vehicle2.Position.Y < 0)
-                {
-                    vehicle2.Position.X = 758;
-                    vehicle2.Position.Y = 70;
-                }
-                //Hier word gecheckt als de voertuig zich mag verplaatsen op de locatie
-                if (vehicle1.Position.X > 0 & vehicle1.Position.Y > 0 & vehicle1.Position.X < 990 & vehicle1.Position.Y < 700)
-                {
-                    vehicle1.Position.X += vehicle1.Speed.X;
-                    vehicle1.Position.Y += vehicle1.Speed.Y;
-                }
-                //Als de voertuig tegen de grens aanzit word hij gestopt
-                else
-                {
-                    vehicle1.Position.X -= vehicle1.Speed.X;
-                    vehicle1.Position.Y -= vehicle1.Speed.Y;
-                }
-                //Hier word gecheckt als de voertuig zich mag verplaatsen op de locatie
-                if (vehicle2.Position.X > 0 & vehicle2.Position.Y > 0 & vehicle2.Position.X < 990 & vehicle2.Position.Y < 700)
-                {
-                    vehicle2.Position.X += vehicle2.Speed.X;
-                    vehicle2.Position.Y += vehicle2.Speed.Y;
-                }
-                //Als de voertuig tegen de grens aanzit word hij gestopt
-                else
-                {
-                    vehicle2.Position.X -= vehicle2.Speed.X;
-                    vehicle2.Position.Y -= vehicle2.Speed.Y;
-                }
+                vehicle1.checklocation();
+                vehicle2.checklocation();
                 //De auto(als in voertuig) debugger! deze controleerd nu op de locatie
                 MousePosition.Text = (Convert.ToString(vehicle1.Position));
                 //Tegen het kapot gaan van onze game
-                try
-                {
-                    //wat is de kleur van de huidige positie
-                    //Checkt de kleur waar vehicle1 op positie staat(We moeten echt de GUI afschafen van Visual studio :p)
-                    //Controleer de juist locatie
-                    var image = new Bitmap(@"C:\Users\robert\Documents\GitHub\Racegame\Racegame\Racegame\Racegame\Resources\Naamloos.jpg");
-                    //Een if statement voor een onnodige exception
-                    if (vehicle1.Position.X < -10 | vehicle1.Position.Y < -10 | vehicle1.Position.X < 1000 | vehicle1.Position.Y < 710)
-                    {
-                        Color kleur = image.GetPixel(vehicle1.Position.X, vehicle1.Position.Y);
-                        //Debug geeft de code waar hij op staat
-                        Console.WriteLine(kleur);
-                        //Gras
-                        if (kleur.A.Equals(255) && kleur.R.Equals(0) && kleur.G.Equals(128) && kleur.B.Equals(1))
-                            Console.WriteLine("Gras");
-                        //De weg
-                        if (kleur.A.Equals(255) && kleur.R.Equals(106) && kleur.G.Equals(104) && kleur.B.Equals(105))
-                            Console.WriteLine("weg");
-                        //Pitstop
-                        if (kleur.A.Equals(255) && kleur.R.Equals(0) && kleur.G.Equals(0) && kleur.B.Equals(206)) 
-                            Console.WriteLine("Pitstop");
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Error met de bitmap: " + e);
-                }
-            
+                vehicle1.getColor();
+                vehicle1.getColor();
                 Invalidate();
             }
         }
--- Racegame/Racegame/Racegame/Vehicle.cs
+++ Racegame/Racegame/Racegame/Vehicle.cs
@@ -15,7 +15,7 @@ namespace Racegame
         public Point Speed;
         public int CarSize { get; set; }
         public int AxisSpeed { get; set; }
-
+        public Color kleur { get; set; }
         /// <summary>
         /// De constructor
         /// </summary>
@@ -46,6 +46,57 @@ namespace Racegame
                 g.FillRectangle(Brushes.Red, this.Position.X - this.CarSize / 2, this.Position.Y - this.CarSize / 2, this.CarSize, this.CarSize);
             if (b == "yellow")
                 g.FillRectangle(Brushes.Yellow, this.Position.X - this.CarSize / 2, this.Position.Y - this.CarSize / 2, this.CarSize, this.CarSize);
+        }
+        public void checklocation()
+        {
+            //hier check je of de autos niet van het scherm rijden/ Als de voertuig "Te verweg is van de baan" word hij op de start positie gezet
+            if (Position.X < -10 | Position.Y < -10 | Position.X > 1000 | Position.Y > 710)
+            {
+                Position.X = 768;
+                Position.Y = 70;
+            }
+            //Hier word gecheckt als de voertuig zich mag verplaatsen op de locatie
+            if (Position.X > 0 & Position.Y > 0 & Position.X < 990 & Position.Y < 700)
+            {
+                Position.X += Speed.X;
+                Position.Y += Speed.Y;
+            }
+            //Als de voertuig tegen de grens aanzit word hij gestopt
+            else
+            {
+                Position.X -= Speed.X;
+                Position.Y -= Speed.Y;
+            }
+        }
+        public void getColor()
+        {
+            try
+            {
+                //wat is de kleur van de huidige positie
+                //Checkt de kleur waar vehicle1 op positie staat(We moeten echt de GUI afschafen van Visual studio :p)
+                //Controleer de juist locatie
+                var image = new Bitmap(@"C:\Users\robert\Documents\GitHub\Racegame\Racegame\Racegame\Racegame\Resources\Naamloos.jpg");
+                //Een if statement voor een onnodige exception
+                if (Position.X < -10 | Position.Y < -10 | Position.X < 1000 | Position.Y < 710)
+                {
+                    Color kleur = image.GetPixel(Position.X, Position.Y);
+                    //Debug geeft de code waar hij op staat
+                    Console.WriteLine(kleur);
+                    //Gras
+                    if (kleur.A.Equals(255) && kleur.R.Equals(0) && kleur.G.Equals(128) && kleur.B.Equals(1))
+                        Console.WriteLine("Gras");
+                    //De weg
+                    if (kleur.A.Equals(255) && kleur.R.Equals(106) && kleur.G.Equals(104) && kleur.B.Equals(105))
+                        Console.WriteLine("weg");
+                    //Pitstop
+                    if (kleur.A.Equals(255) && kleur.R.Equals(0) && kleur.G.Equals(0) && kleur.B.Equals(206))
+                        Console.WriteLine("Pitstop");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error met de bitmap: " + e);
+            }
         }
     }
 }
